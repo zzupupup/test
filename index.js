81f95892675a4c254916be151b7df9f13a89ccc3
azhi <963085900@qq.com>
2021-06-11 11:01:13 +0800

    更新

--- index.js
+++ index.js
@@ -39,13 +39,14 @@ const getStatus = ()=>{
 }
 
 // 处理数据结果
-function dealData(checkInRes, statusRes){
+function dealData(checkInRes, statusRes, during){
   return {
     message: checkInRes.message,
     // checkInRec: checkInRes.list,
     useDays: statusRes.days,
     email: statusRes.email,
     leftDays: parseFloat(statusRes.leftDays).toFixed(),
+    during
   }
 }
 
@@ -62,30 +63,55 @@ function pushMessage(content){
   });
 }
 
-function prettyRes(res, during){
-  let {useDays, leftDays, message, email} = res;
+function prettyRes(res, cnt){
+  let {useDays, leftDays, message, email, during} = res;
   let content = "<table border='1px solid black'><thead><tr><td>key</td><td>value</td></tr></thead><tbody>";
   let tr1 = "<tr><td>useDays</td><td>" + useDays + "</td></tr>";
   let tr2 = "<tr><td>leftDays</td><td>" + leftDays + "</td></tr>";
   let tr3 = "<tr><td>email</td><td>" + email + "</td></tr>";
   let tr4 = "<tr><td>message</td><td>" + message + "</td></tr>";
-  let tr5 = "<tr><td>cost</td><td>" + during + "</td></tr>";
+  let tr5 = "<tr><td>cost</td><td>try " + cnt + " times, during"+ during + "s</td></tr>";
   let tail = "</tbody></table>";
   content += (tr1 + tr2 + tr3 + tr4 + tr5 + tail);
-  
-  return content
+  return content;
 }
 
-async function main(){
+
+async function checkOnce(){
   let start = new Date();
   const checkInRes =  await checkIn();
-  const StatusRes = await getStatus();
+  const statusRes = await getStatus();
   let end = new Date();
   let during = (end - start) / 1000;
-  let res = dealData(checkInRes.data, StatusRes.data.data);
-  console.log(res);
-  console.log("done, use " + during + "s");
-  let content = prettyRes(res, during);
+  return{
+    checkInRes: checkInRes.data,
+    statusRes: statusRes.data.data, 
+    during
+  }
+}
+async function main(){
+
+  let cnt = 0;
+  const MAX_CNT = 10;
+  let res = null;
+  while(cnt < MAX_CNT){
+    let checkOnceRes = await checkOnce();
+    res = dealData(checkOnceRes.checkInRes, checkOnceRes.statusRes, checkOnceRes.during);
+    console.log("第" + cnt + "次签到");
+    console.log(res);
+    if(res.code == 0){
+      // 签到成功
+      break;
+    }else if(res.message == 'Please Try Tomorrow'){
+      // 签到过了
+      console.log("签到过了")
+      break;
+    }else{
+      cnt ++;
+    }
+  }
+ 
+  let content = prettyRes(res, cnt + 1);
   pushMessage(content);
 }
 
